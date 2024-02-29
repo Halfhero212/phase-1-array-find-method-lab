153a446bd7f43200827303621f896e2fcef835ab
Nooralhussein <lighthero212@gmail.com>
2024-02-29 14:52:06 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    // add more records as necessary
+  ];
+  
+  function superbowlWin(record) {
+    const win = record.find(record => record.result === "W");
+    return win ? win.year : undefined;
+  }
+  
+  console.log(superbowlWin(record)); // This should log the year of the win if there is one, or undefined if there isn't.
+  
